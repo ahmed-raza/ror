3568be74690315de14b4b5d66ac88e067f3cb3e8
Ahmed Raza <ahmed.raza@square63.com>
2016-05-02 16:36:46 +0500

    First ROR site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'ab998387fcd088667ac5e450d907b1b6a37f708a6463542e3352b25700a35e06332348a1fce036478d50232aad2cb8f33c84685fbde1a7acfbcfaed6de26a869'
