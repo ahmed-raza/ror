e7646d3f0d6d7e442a82bc6668825e00c8fda7b2
Ahmed Raza <ahmed.raza@square63.com>
2016-05-25 14:01:54 +0500

    Created: Orders model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20160525073920) do
+ActiveRecord::Schema.define(:version => 20160525090122) do
 
   create_table "articles", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20160525073920) do
     t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "tags"
   end
 
   create_table "comments", :force => true do |t|
@@ -30,6 +31,12 @@ ActiveRecord::Schema.define(:version => 20160525073920) do
   end
 
   add_index "comments", ["article_id"], :name => "index_comments_on_article_id"
+
+  create_table "customers", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "models", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -48,6 +55,12 @@ ActiveRecord::Schema.define(:version => 20160525073920) do
 
   add_index "models", ["email"], :name => "index_models_on_email", :unique => true
   add_index "models", ["reset_password_token"], :name => "index_models_on_reset_password_token", :unique => true
+
+  create_table "orders", :force => true do |t|
+    t.string   "order_name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "title"
