3891f7dd55ebc70c72323c882e987401d274654c
Ahmed Raza <ahmed.raza@square63.com>
2016-06-14 10:21:17 +0500

    Validations and errors

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,9 @@
 class Article < ActiveRecord::Base
   attr_accessible :content, :name, :title
 
-  validates :name, :title, :presence => true
+  validates :name, :title, :content, :presence => { :message => "is empty" }
   validates :title, :uniqueness => true, :length => { :minimum => 5 }
+  validates :name, :format => {:with => /\A[a-z][a-z0-9_]+\Z/}
   has_many :comments
 
   scope :aurther_name,lambda { |name| where(:name => name) }
